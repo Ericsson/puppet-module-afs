04afe12cbaeeedeec996d80020aae39e9fdd6615
Phil Friderici (ephifre) <phil.friderici@ericsson.com>
2015-03-20 12:31:39 +0100

    add Ubuntu support for custom fact afs_version

--- lib/facter/afs_version.rb
+++ lib/facter/afs_version.rb
@@ -7,5 +7,10 @@ Facter.add("afs_version") do
       cmd = "rpm -q --queryformat='%{VERSION}' openafs"
       response = Facter::Util::Resolution.exec(cmd)
     end
+    dpkg_test_exists = "dpkg-query -W --showformat='${version}' openafs-client 2>&1 >/dev/null ; echo $?"
+    if Facter::Util::Resolution.exec(dpkg_test_exists) == '0'
+      cmd = "dpkg-query -W --showformat='${version}' openafs-client"
+      response = Facter::Util::Resolution.exec(cmd)
+    end
   end
 end
