033cfdf1c4e2663b63ff8f49bc411fd6c084a63e
Daniel Tremblay <daniel.tremblay@ericsson.com>
2016-06-08 14:45:59 -0400

    Add Solaris support for afs_version fact

--- lib/facter/afs_version.rb
+++ lib/facter/afs_version.rb
@@ -1,15 +1,33 @@
 # afs_version.rb
 
-Facter.add("afs_version") do
-  setcode do
-    rpm_test_exists = "rpm -q openafs 2>&1 >/dev/null ; echo $?"
-    dpkg_test_exists = "dpkg-query -W --showformat='${version}' openafs-client 2>&1 >/dev/null ; echo $?"
-    if Facter::Util::Resolution.exec(rpm_test_exists) == '0'
-      cmd = "rpm -q --queryformat='%{VERSION}' openafs"
-      response = Facter::Util::Resolution.exec(cmd)
-    elsif Facter::Util::Resolution.exec(dpkg_test_exists) == '0'
-      cmd = "dpkg-query -W --showformat='${version}' openafs-client"
-      response = Facter::Util::Resolution.exec(cmd)
+Facter.add('afs_version') do
+  confine :kernel => 'SunOS'
+
+    setcode do
+      begin
+        pkg_exists = "pkginfo EISopenafs >/dev/null 2>&1 ; echo $?"
+      if Facter::Util::Resolution.exec(pkg_exists) == '0'
+        cmd = "pkginfo -l EISopenafs | grep VERSION | awk '{print $NF}'"
+        response = Facter::Util::resolution.exec(cmd)
+      end
+    end
+  end
+end
+
+Facter.add('afs_version') do
+  confine :kernel => 'Linux'
+
+    setcode do
+      begin
+        rpm_test_exists = "rpm -q openafs 2>&1 >/dev/null ; echo $?"
+        dpkg_test_exists = "dpkg-query -W --showformat='${version}' openafs-client 2>&1 >/dev/null ; echo $?"
+      if Facter::Util::Resolution.exec(rpm_test_exists) == '0'
+        cmd = "rpm -q --queryformat='%{VERSION}' openafs"
+        response = Facter::Util::Resolution.exec(cmd)
+      elsif Facter::Util::Resolution.exec(dpkg_test_exists) == '0'
+        cmd = "dpkg-query -W --showformat='${version}' openafs-client"
+        response = Facter::Util::Resolution.exec(cmd)
+      end
     end
   end
 end
